db4ded5784d7b832caecb704e56b9fe7329262ea
Ventura Del Monte <venturadelmonte@gmail.com>
2017-08-12 12:48:50 +0200

    Add GDELT parser

---
+++ hackathon-common/src/main/java/eu/streamline/hackathon/common/GDELTParser.java
@@ -0,0 +1,176 @@
+package eu.streamline.hackathon.common;
+
+import eu.streamline.hackathon.common.data.GDELTEvent;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GDELTParser implements Serializable {
+
+	private static final String DELIMITER = "\t";
+    private static final DateFormat DF_GDELT = new SimpleDateFormat("yyyyMMdd");
+
+    private ArrayList<Field> fields;
+
+
+    private ArrayList<Field> generateGeo() {
+    	ArrayList<Field> geo = new ArrayList<Field>();
+		geo.add(new Field("type", Integer.class));
+		geo.add(new Field("name", String.class));
+		geo.add(new Field("countryCode", String.class));
+		geo.add(new Field("adm1Code", String.class));
+		geo.add(new Field("lat", Double.class));
+		geo.add(new Field("long", Double.class));
+		geo.add(new Field("featureId", Integer.class));
+		return geo;
+	}
+
+	 private ArrayList<Field> generateCodes() {
+		ArrayList<Field> codes = new ArrayList<>();
+		codes.add(new Field("code", String.class));
+		codes.add(new Field("name", String.class));
+		codes.add(new Field("countryCode", String.class));
+		codes.add(new Field("knownGroupCode", String.class));
+		codes.add(new Field("ethnicCode", String.class));
+		codes.add(new Field("religion1Code", String.class));
+		codes.add(new Field("religion2Code", String.class));
+		codes.add(new Field("type1Code", String.class));
+		codes.add(new Field("type2Code", String.class));
+		codes.add(new Field("type3Code", String.class));
+		return codes;
+	}
+
+
+	public GDELTParser() {
+
+		fields = new ArrayList<>();
+
+		// Actor1, Actor2
+		fields.add(new Field("cameo", Integer.class));
+		fields.add(new Field("day", Date.class));
+		fields.add(new Field("month", Integer.class));
+		fields.add(new Field("year", Integer.class));
+		fields.add(new Field("fracDate", Double.class));
+		fields.add(new Field("actor1Code", generateCodes()));
+		fields.add(new Field("actor2Code", generateCodes()));
+
+		// Common
+		fields.add(new Field("isRoot", Boolean.class));
+		fields.add(new Field("eventCode", String.class));
+		fields.add(new Field("eventBaseCode", String.class));
+		fields.add(new Field("eventRootCode", String.class));
+		fields.add(new Field("quadClass", Integer.class));
+		fields.add(new Field("goldstein", Double.class));
+		fields.add(new Field("numMentions", Integer.class));
+		fields.add(new Field("numSources", Integer.class));
+		fields.add(new Field("numArticles", Integer.class));
+		fields.add(new Field("avgTone", Double.class));
+		fields.add(new Field("actor1Geo", generateGeo()));
+		fields.add(new Field("actor2Geo", generateGeo()));
+		fields.add(new Field("eventGeo", generateGeo()));
+		fields.add(new Field("dateAdded", Integer.class));
+		fields.add(new Field("sourceUrl", String.class));
+
+	}
+
+	public GDELTEvent readRecord(String input) throws Exception {
+
+		String[] tokens = input.split(DELIMITER);
+
+		for(int i = 0, j = 0 ; i < tokens.length; j++){
+			Field curr = fields.get(j);
+			if (curr.hasChildren()) {
+				for (Field child : curr.getChildren()) {
+					child.setValue(tokens[i++]);
+				}
+			} else {
+				curr.setValue(tokens[i++]);
+			}
+		}
+
+		GDELTEvent event = new GDELTEvent();
+		Class<?> clz = event.getClass();
+
+		try {
+			for (Field field : fields) {
+				if (field.hasChildren()) {
+					for (Field child : field.getChildren()) {
+						clz.getField(field.name + "_" + child.name).set(event, child.value);
+					}
+				} else {
+					clz.getField(field.name).set(event, field.value);
+				}
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+
+		return event;
+	}
+
+
+	
+	private class Field implements Serializable {
+
+		private String name;
+		private Class<?> clazz;
+		private Object value;
+		private ArrayList<Field> children;
+
+
+		public Field(String name, Class<?> clazz) {
+			this.name = name;
+			this.clazz = clazz;
+		}
+		
+		public Field(String name, ArrayList<Field> children) {
+			this.name = name;
+			this.children = children;
+		}
+
+		public boolean hasChildren() {
+			return children != null && children.size() > 0;
+		}
+
+		public ArrayList<Field> getChildren() {
+			return children;
+		}
+
+
+		public void setValue(String str) {
+			if (clazz == null && children.size() == 0){
+				throw new IllegalArgumentException("Class not supplied for token " + name);
+			}
+			if (str == null || str.equals("")) {
+				value = null;
+				return;
+			}
+			try {
+				if (clazz.equals(Integer.class)) {
+					value = Integer.parseInt(str);
+				} else if (clazz.equals(Double.class)) {
+					value =  Double.parseDouble(str);
+				} else if (clazz.equals(Date.class)) {
+					value = DF_GDELT.parse(str);
+				} else if (clazz.equals(Boolean.class)) {
+					value = str.equals("1");
+				} else if (clazz.equals(String.class)) {
+					value = str;
+				} else {
+					value = null;
+				}
+			} catch (Exception ex) {
+				value = null;
+			}
+
+		}
+
+	}
+
+
+
+
+}
---
+++ hackathon-common/src/main/java/eu/streamline/hackathon/common/data/GDELTEvent.java
@@ -0,0 +1,132 @@
+package eu.streamline.hackathon.common.data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class GDELTEvent implements Serializable {
+
+	// this code is automatic generated - DO NOT EDIT IT!
+
+	public Integer cameo;
+	public Date day;
+	public Integer month;
+	public Integer year;
+	public Double fracDate;
+	public Boolean isRoot;
+	public String eventCode;
+	public String eventBaseCode;
+	public String eventRootCode;
+	public Integer quadClass;
+	public Double goldstein;
+	public Integer numMentions;
+	public Integer numSources;
+	public Integer numArticles;
+	public Double avgTone;
+	public Integer dateAdded;
+	public String sourceUrl;
+	public String actor1Code_code;
+	public String actor2Code_code;
+	public String actor1Code_name;
+	public String actor2Code_name;
+	public String actor1Code_countryCode;
+	public String actor2Code_countryCode;
+	public String actor1Code_knownGroupCode;
+	public String actor2Code_knownGroupCode;
+	public String actor1Code_ethnicCode;
+	public String actor2Code_ethnicCode;
+	public String actor1Code_religion1Code;
+	public String actor2Code_religion1Code;
+	public String actor1Code_religion2Code;
+	public String actor2Code_religion2Code;
+	public String actor1Code_type1Code;
+	public String actor2Code_type1Code;
+	public String actor1Code_type2Code;
+	public String actor2Code_type2Code;
+	public String actor1Code_type3Code;
+	public String actor2Code_type3Code;
+	public Integer actor1Geo_type;
+	public Integer actor2Geo_type;
+	public Integer eventGeo_type;
+	public String actor1Geo_name;
+	public String actor2Geo_name;
+	public String eventGeo_name;
+	public String actor1Geo_countryCode;
+	public String actor2Geo_countryCode;
+	public String eventGeo_countryCode;
+	public String actor1Geo_adm1Code;
+	public String actor2Geo_adm1Code;
+	public String eventGeo_adm1Code;
+	public Double actor1Geo_lat;
+	public Double actor2Geo_lat;
+	public Double eventGeo_lat;
+	public Double actor1Geo_long;
+	public Double actor2Geo_long;
+	public Double eventGeo_long;
+	public Integer actor1Geo_featureId;
+	public Integer actor2Geo_featureId;
+	public Integer eventGeo_featureId;
+
+	@Override
+	public String toString() {
+		return "GDELTEvent{" +
+				"cameo=" + cameo +
+				", day=" + day +
+				", month=" + month +
+				", year=" + year +
+				", fracDate=" + fracDate +
+				", isRoot=" + isRoot +
+				", eventCode='" + eventCode + '\'' +
+				", eventBaseCode='" + eventBaseCode + '\'' +
+				", eventRootCode='" + eventRootCode + '\'' +
+				", quadClass=" + quadClass +
+				", goldstein=" + goldstein +
+				", numMentions=" + numMentions +
+				", numSources=" + numSources +
+				", numArticles=" + numArticles +
+				", avgTone=" + avgTone +
+				", dateAdded=" + dateAdded +
+				", sourceUrl='" + sourceUrl + '\'' +
+				", actor1Code_code='" + actor1Code_code + '\'' +
+				", actor2Code_code='" + actor2Code_code + '\'' +
+				", actor1Code_name='" + actor1Code_name + '\'' +
+				", actor2Code_name='" + actor2Code_name + '\'' +
+				", actor1Code_countryCode='" + actor1Code_countryCode + '\'' +
+				", actor2Code_countryCode='" + actor2Code_countryCode + '\'' +
+				", actor1Code_knownGroupCode='" + actor1Code_knownGroupCode + '\'' +
+				", actor2Code_knownGroupCode='" + actor2Code_knownGroupCode + '\'' +
+				", actor1Code_ethnicCode='" + actor1Code_ethnicCode + '\'' +
+				", actor2Code_ethnicCode='" + actor2Code_ethnicCode + '\'' +
+				", actor1Code_religion1Code='" + actor1Code_religion1Code + '\'' +
+				", actor2Code_religion1Code='" + actor2Code_religion1Code + '\'' +
+				", actor1Code_religion2Code='" + actor1Code_religion2Code + '\'' +
+				", actor2Code_religion2Code='" + actor2Code_religion2Code + '\'' +
+				", actor1Code_type1Code='" + actor1Code_type1Code + '\'' +
+				", actor2Code_type1Code='" + actor2Code_type1Code + '\'' +
+				", actor1Code_type2Code='" + actor1Code_type2Code + '\'' +
+				", actor2Code_type2Code='" + actor2Code_type2Code + '\'' +
+				", actor1Code_type3Code='" + actor1Code_type3Code + '\'' +
+				", actor2Code_type3Code='" + actor2Code_type3Code + '\'' +
+				", actor1Geo_type=" + actor1Geo_type +
+				", actor2Geo_type=" + actor2Geo_type +
+				", eventGeo_type=" + eventGeo_type +
+				", actor1Geo_name='" + actor1Geo_name + '\'' +
+				", actor2Geo_name='" + actor2Geo_name + '\'' +
+				", eventGeo_name='" + eventGeo_name + '\'' +
+				", actor1Geo_countryCode='" + actor1Geo_countryCode + '\'' +
+				", actor2Geo_countryCode='" + actor2Geo_countryCode + '\'' +
+				", eventGeo_countryCode='" + eventGeo_countryCode + '\'' +
+				", actor1Geo_adm1Code='" + actor1Geo_adm1Code + '\'' +
+				", actor2Geo_adm1Code='" + actor2Geo_adm1Code + '\'' +
+				", eventGeo_adm1Code='" + eventGeo_adm1Code + '\'' +
+				", actor1Geo_lat=" + actor1Geo_lat +
+				", actor2Geo_lat=" + actor2Geo_lat +
+				", eventGeo_lat=" + eventGeo_lat +
+				", actor1Geo_long=" + actor1Geo_long +
+				", actor2Geo_long=" + actor2Geo_long +
+				", eventGeo_long=" + eventGeo_long +
+				", actor1Geo_featureId=" + actor1Geo_featureId +
+				", actor2Geo_featureId=" + actor2Geo_featureId +
+				", eventGeo_featureId=" + eventGeo_featureId +
+				'}';
+	}
+}
